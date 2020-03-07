6cf356572adedf8c092b7e6a1a32eb2fb3d72116
theoriginalbit <theoriginalbit@users.noreply.github.com>
2020-03-07 17:27:03 +1100

    Add first and probably only working version

--- Sources/UIButtonFeedbackExtension/UIButtonFeedbackExtension.swift
+++ Sources/UIButtonFeedbackExtension/UIButtonFeedbackExtension.swift
@@ -1,3 +1,65 @@
-struct UIButtonFeedbackExtension {
-    var text = "Hello, World!"
+import UIKit
+
+public extension UIButton {
+    private enum AssociatedKeys {
+        static var feedbackGenerator = "feedbackGenerator"
+        static var feedbackOnTouchUp = "feedbackOnTouchUp"
+        static var feedbackOnTouchDown = "feedbackOnTouchDown"
+    }
+
+    private(set) var feedbackGenerator: UISelectionFeedbackGenerator? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.feedbackGenerator) as? UISelectionFeedbackGenerator
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.feedbackGenerator, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+
+    var feedbackOnTouchUp: Bool {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.feedbackOnTouchUp) as? Bool ?? false
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.feedbackOnTouchUp, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            refreshFeedbackGenerator()
+        }
+    }
+
+    var feedbackOnTouchDown: Bool {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.feedbackOnTouchDown) as? Bool ?? false
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.feedbackOnTouchDown, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            refreshFeedbackGenerator()
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+
+        if feedbackOnTouchDown {
+            feedbackGenerator?.selectionChanged()
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+
+        if feedbackOnTouchUp {
+            feedbackGenerator?.selectionChanged()
+        }
+    }
+
+    private func refreshFeedbackGenerator() {
+        switch (feedbackOnTouchUp, feedbackOnTouchDown, feedbackGenerator) {
+        case (true, _, .none), (_, true, .none): // if either are enabled and we don't have a generator, make one
+            feedbackGenerator = UISelectionFeedbackGenerator()
+        case (false, false, .some): // if both are disabled and we do have a generator, clear it
+            feedbackGenerator = nil
+        case (_, _, _): // no change required
+            break
+        }
+    }
 }
